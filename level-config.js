c8fb6d98891db15a030f67f03de4ca3ff9170782
maxiangmei2-debug <maxiangmei2-debug@users.noreply.github.com>
2026-08-04 15:51:51 +0800

    chore: 部署英语学习中心更新 (v20260804-1551)

--- level-config.js
+++ level-config.js
@@ -62,7 +62,7 @@
       '<button class="lb-btn" data-lv="B1">B1</button>' +
       '<button class="lb-btn" data-lv="B2">B2</button>' +
       '<span class="lb-sep"></span>' +
-      '<a class="lb-home" href="index.html?v=20260804-1419">⌂ 学习中心</a>';
+      '<a class="lb-home" href="index.html?v=20260804-1551">⌂ 学习中心</a>';
     document.body.insertBefore(bar, document.body.firstChild);
     document.body.classList.add('has-level-bar');
     var btns = bar.querySelectorAll('.lb-btn');
